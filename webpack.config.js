fb03316357e7ffc2e59ffad42cc1bd27c12720aa
HarshwardhanSingh <harsh.rathore14@gmail.com>
2018-03-27 00:31:54 +0530

    setup bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,47 @@ module.exports = {
         test: /\.js?$/,
         loader: 'babel-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.css?$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ]
+      },
+      {
+        test: /\.scss?$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('precss'),
+                  require('autoprefixer')
+                ];
+              }
+            }
+          },
+          {
+            loader: 'sass-loader',
+          }
+        ]
       }
     ]
+  },
+  resolve: {
+    extensions: ['.js'],
   },
   plugins: [
     new HtmlWebpackPlugin({
